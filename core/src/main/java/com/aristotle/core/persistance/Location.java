7c37dc2a061abb4e7a7b412b3a11a85e3d995892
Rishi Renjith <rrenjith@yahoo-inc.com>
2015-05-18 18:18:48 +0100

    getters and setters for location

--- core/src/main/java/com/aristotle/core/persistance/Location.java
+++ core/src/main/java/com/aristotle/core/persistance/Location.java
@@ -31,6 +31,50 @@ public class Location {
     private Location parentLocation;
     @Column(name="parent_id", nullable = true)
     private String parentId;
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public LocationType getLocationType() {
+        return locationType;
+    }
+    public void setLocationType(LocationType locationType) {
+        this.locationType = locationType;
+    }
+    public String getLocation_type_id() {
+        return location_type_id;
+    }
+    public void setLocation_type_id(String location_type_id) {
+        this.location_type_id = location_type_id;
+    }
+    public Location getParentLocation() {
+        return parentLocation;
+    }
+    public void setParentLocation(Location parentLocation) {
+        this.parentLocation = parentLocation;
+    }
+    public String getParentId() {
+        return parentId;
+    }
+    public void setParentId(String parentId) {
+        this.parentId = parentId;
+    }
+    @Override
+    public String toString() {
+        return "Location [id=" + id + ", name=" + name + ", locationType="
+                + locationType + ", location_type_id=" + location_type_id
+                + ", parentLocation=" + parentLocation + ", parentId="
+                + parentId + "]";
+    }
+    
     
     
 }
