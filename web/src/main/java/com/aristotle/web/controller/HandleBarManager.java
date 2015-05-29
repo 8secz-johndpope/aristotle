66c94663adec949fef96f5d207cb2cba68acbe4a
Ravi Sharma <ping2ravi@gmail.com>
2015-05-29 14:19:55 +0100

    added Helper to check if index can be divided by given number

--- web/src/main/java/com/aristotle/web/controller/HandleBarManager.java
+++ web/src/main/java/com/aristotle/web/controller/HandleBarManager.java
@@ -26,6 +26,7 @@ public class HandleBarManager {
     @PostConstruct
     public void init() {
         registerTrimStringFunction();
+        registerIfIndexDividedByFunction();
     }
 
     private void registerTrimStringFunction() {
@@ -37,6 +38,21 @@ public class HandleBarManager {
                     return context.subSequence(0, 300);
                 }
                 return context;
+            }
+
+        });
+    }
+
+    private void registerIfIndexDividedByFunction() {
+        handlebars.registerHelper("ifIndexDividedBy", new Helper<String>() {
+            @Override
+            public CharSequence apply(String context, Options options) throws IOException {
+                int currentIndex = (Integer) options.params[0];
+                int divisor = (Integer) options.params[1];
+                if (currentIndex % divisor == 0) {
+                    return "true".subSequence(0, 3);
+                }
+                return "false".subSequence(0, 3);
             }
 
         });
