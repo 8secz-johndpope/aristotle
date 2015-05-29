61a67d3b8f9bcd4389e4f8a8d22bb2ce9d113ec9
Ravi Sharma <ping2ravi@gmail.com>
2015-05-29 23:45:08 +0100

    Fixed Url for Facebook Comment plugin

--- web/src/main/java/com/aristotle/web/plugin/impl/FacebookCommentPlugin.java
+++ web/src/main/java/com/aristotle/web/plugin/impl/FacebookCommentPlugin.java
@@ -23,13 +23,14 @@ public class FacebookCommentPlugin extends AbstractDataPlugin {
 
     @Override
     public void applyPlugin(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, ModelAndView mv) {
+        String requestedUrl = httpServletRequest.getRequestURL().toString();
         String fbData = "<div id=\"fb-root\"></div><script>(function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0];"
                 + " if (d.getElementById(id)) return;"
                 + " js = d.createElement(s); js.id = id;"
                 + " js.src = \"//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3&appId=119862768108344\";"
                 + " fjs.parentNode.insertBefore(js, fjs);"
                 + " }(document, 'script', 'facebook-jssdk'));</script>";
-        String commentBox = " <div class=\"fb-comments\" data-href=\"http://developers.facebook.com/docs/plugins/comments/\" data-numposts=\"5\" data-colorscheme=\"light\"></div>";
+        String commentBox = " <div class=\"fb-comments\" data-href=\"" + requestedUrl + "\" data-numposts=\"5\" data-colorscheme=\"light\"></div>";
         JsonObject context = (JsonObject) mv.getModel().get("context");
         context.addProperty(name, fbData + commentBox);
 
