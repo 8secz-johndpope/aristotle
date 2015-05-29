ecaab83494961ddf5e3eb0b925c6e4e8c5455ca1
Ravi Sharma <ping2ravi@gmail.com>
2015-05-29 13:57:05 +0100

    Data Plugin can be made global which will be applied to all Urls

--- core/src/main/java/com/aristotle/core/persistance/DataPlugin.java
+++ core/src/main/java/com/aristotle/core/persistance/DataPlugin.java
@@ -39,6 +39,8 @@ public class DataPlugin {
     private boolean disabled;
     @Column(name = "plugin_name")
     private String pluginName;
+    @Column(name = "global", columnDefinition = "BIT(1) DEFAULT 0")
+    private boolean global;
 
     public Long getId() {
         return id;
@@ -102,6 +104,14 @@ public class DataPlugin {
 
     public void setPluginName(String pluginName) {
         this.pluginName = pluginName;
+    }
+
+    public boolean isGlobal() {
+        return global;
+    }
+
+    public void setGlobal(boolean global) {
+        this.global = global;
     }
 
     @Override
