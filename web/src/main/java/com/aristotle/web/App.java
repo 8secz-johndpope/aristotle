d3a146eeaeac69f7ab3aecfbf22620da711df969
Ravi Sharma <ping2ravi@gmail.com>
2015-06-02 05:34:57 +0100

    Added video cache

--- web/src/main/java/com/aristotle/web/App.java
+++ web/src/main/java/com/aristotle/web/App.java
@@ -44,7 +44,7 @@ public class App extends SpringBootServletInitializer {
     @Bean
     public CacheManager cacheManager() {
         CacheBuilder cacheBuilder = CacheBuilder.newBuilder().expireAfterWrite(5, TimeUnit.MINUTES);
-        GuavaCacheManager guavaCacheManager = new GuavaCacheManager("events");
+        GuavaCacheManager guavaCacheManager = new GuavaCacheManager("events", "videos");
         guavaCacheManager.setCacheBuilder(cacheBuilder);
         return guavaCacheManager;
     }
