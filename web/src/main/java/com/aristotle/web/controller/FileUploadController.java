818df9949ed515267a069e015792336301fede6d
Ravi Sharma <ping2ravi@gmail.com>
2015-07-08 05:38:52 +0100

    Fixed error

--- web/src/main/java/com/aristotle/web/controller/FileUploadController.java
+++ web/src/main/java/com/aristotle/web/controller/FileUploadController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.portlet.ModelAndView;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
 import com.aristotle.core.persistance.User;
