83f0669107edd704a20c0a53fe9cee1dd7130e36
Ravi Sharma <ping2ravi@gmail.com>
2015-08-03 07:24:30 +0100

    Added more fields

--- core/src/main/java/com/aristotle/core/service/dto/IVrUserRegisterBean.java
+++ core/src/main/java/com/aristotle/core/service/dto/IVrUserRegisterBean.java
@@ -4,10 +4,17 @@ package com.aristotle.core.service.dto;
 public class IVrUserRegisterBean {
 	
     private String mobileNumber;
+    private String membershipNumber;
     private String countryCode;
 
     private String adminMobileNumber;
     private String adminCountryCode;
+
+    private String name;
+    private String state;
+    private String district;
+    private String ac;
+    private String pc;
 
     public String getMobileNumber() {
         return mobileNumber;
@@ -41,6 +48,53 @@ public class IVrUserRegisterBean {
         this.adminCountryCode = adminCountryCode;
     }
 
+    public String getMembershipNumber() {
+        return membershipNumber;
+    }
+
+    public void setMembershipNumber(String membershipNumber) {
+        this.membershipNumber = membershipNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getAc() {
+        return ac;
+    }
+
+    public void setAc(String ac) {
+        this.ac = ac;
+    }
+
+    public String getPc() {
+        return pc;
+    }
+
+    public void setPc(String pc) {
+        this.pc = pc;
+    }
 
 
 }
