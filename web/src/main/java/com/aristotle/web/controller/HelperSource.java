55537786c26b561b8d7b5e73ea752d52bce95c4c
Ravi Sharma <ping2ravi@gmail.com>
2016-01-29 08:38:26 +0000

    Update HelperSource.java
    Exception handling

--- web/src/main/java/com/aristotle/web/controller/HelperSource.java
+++ web/src/main/java/com/aristotle/web/controller/HelperSource.java
@@ -5,11 +5,15 @@ import java.io.IOException;
 public class HelperSource {
 
     public String trimString(String content, Integer trimLength) throws IOException {
-        if (trimLength == null) {
-            trimLength = 150;
-        }
-        if (content.length() > trimLength) {
-            return content.substring(0, trimLength);
+        try{
+            if (trimLength == null) {
+                trimLength = 150;
+            }
+            if (content.length() > trimLength) {
+                return content.substring(0, trimLength);
+            }
+        }catch{Exception ex){
+            ex.printStackTrace()
         }
         return content;
     }
