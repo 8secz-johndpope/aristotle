f8e4b72960e54611ae4f7ca526f0e213961431ce
Ravi Sharma <ping2ravi@gmail.com>
2016-01-30 23:55:10 +0000

    Donation Related

--- donation/src/main/java/com/aristotle/donation/App.java
+++ donation/src/main/java/com/aristotle/donation/App.java
@@ -1,13 +1,51 @@
 package com.aristotle.donation;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.guava.GuavaCacheManager;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.google.common.cache.CacheBuilder;
+
+@SpringBootApplication
+@ComponentScan(basePackages = { "com.aristotle.core.service", "com.aristotle.web.controller", "com.aristotle.web.plugin.impl", "com.aristotle.web.plugin.impl", "com.aristotle.web.ui.template.impl",
+        "com.aristotle.web.service" })
+@EnableAutoConfiguration
+@EnableCaching
+public class App extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(App.class);
     }
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(new Object[] { App.class, DatabaseConfig.class }, args);
+
+        System.out.println("Let's inspect the beans provided by Spring Boot:");
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            // System.out.println(beanName + " , " + ctx.getBean(beanName));
+            System.out.println(beanName);
+        }
+    }
+
+    @Bean
+    public CacheManager cacheManager() {
+        CacheBuilder cacheBuilder = CacheBuilder.newBuilder().expireAfterWrite(5, TimeUnit.MINUTES);
+        GuavaCacheManager guavaCacheManager = new GuavaCacheManager("list");
+        guavaCacheManager.setCacheBuilder(cacheBuilder);
+        return guavaCacheManager;
+    }
+
 }
