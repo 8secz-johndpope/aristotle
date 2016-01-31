c19c360a2039c5add969503185db9af7b3101c46
Ravi Sharma <ping2ravi@gmail.com>
2016-01-31 00:11:19 +0000

    Added method for LB

--- donation/src/main/java/com/aristotle/donation/controller/HomeController.java
+++ donation/src/main/java/com/aristotle/donation/controller/HomeController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -25,6 +26,12 @@ public class HomeController {
         RedirectView rv = new RedirectView("https://www.instamojo.com/SwarajAbhiyan/donations-for-swaraj-abhiyan/");
         modelAndView.setView(rv);
         return modelAndView;
+    }
+
+    @RequestMapping(value = { "/lb" })
+    @ResponseBody
+    public String forLoadbalancer(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, ModelAndView modelAndView) throws IOException {
+        return "All Good";
     }
 
     
