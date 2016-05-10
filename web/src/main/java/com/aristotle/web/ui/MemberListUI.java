1681d7b5fd955fdc055da33711ad35791d2e032a
Ravi Sharma <ping2ravi@gmail.com>
2016-05-10 09:13:09 +0100

    Update MemberListUI.java

--- web/src/main/java/com/aristotle/web/ui/MemberListUI.java
+++ web/src/main/java/com/aristotle/web/ui/MemberListUI.java
@@ -29,9 +29,8 @@ public class MemberListUI extends UI {
 
 	private final Button addNewBtn;
 
-        @Autowired
-	public MemberListUI(DomainEditor domainEditor) {
-		this.editor = domainEditor;
+	public MemberListUI() {
+		this.editor = new DomainEditor();
 		this.grid = new Grid();
 		this.filter = new TextField();
 		this.addNewBtn = new Button("New Domain", FontAwesome.PLUS);
