bd3d70f6e47c2c330d6098f10be07ff8d35a6ecf
Ravi Sharma <ping2ravi@gmail.com>
2016-05-27 04:43:33 +0100

    f

--- tasks/src/main/java/com/aristotle/task/bolt/sms/SendSms.java
+++ tasks/src/main/java/com/aristotle/task/bolt/sms/SendSms.java
@@ -1,15 +1,13 @@
 package com.aristotle.task.bolt.sms;
 
-import java.util.Date;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-
-import backtype.storm.tuple.Tuple;
 
 import com.aristotle.core.service.SmsService;
 import com.aristotle.task.topology.Result;
 import com.aristotle.task.topology.SpringAwareBaseBolt;
+
+import backtype.storm.tuple.Tuple;
 
 public class SendSms extends SpringAwareBaseBolt {
     private static final long serialVersionUID = 1L;
@@ -24,7 +22,8 @@ public class SendSms extends SpringAwareBaseBolt {
         logInfo("Message Recieved to send Sms");
         while (smsService.sendNextSms()) {
             // Keep processing
-            logInfo("More Sms to process");
+            logInfo("More Sms to process, waiting for 5 seconds before send next one");
+            Thread.sleep(5000);
         }
         return Result.Success;
     }
