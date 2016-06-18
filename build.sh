d8a49ae3f7382b99db6ee49a9127585befe99a42
Ravi Sharma <ping2ravi@gmail.com>
2016-06-18 12:29:52 +0530

    fixed build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,4 @@
-docker stop mysql-for-build-test
-docker rm mysql-for-build-test
-docker run --name=mysql-for-build-test -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=dynamo --net=host -d mysql:5.5
-docker run --name=mysql-for-build-test -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=ss_db_central -p 3304:3306  -d mysql:5.5
-
 cwd=$(pwd)
-
-docker stop AristotleBuild
-docker rm AristotleBuild
-
 
 BUILD_OUTPUT_DIRECTORY=/var/build-output
 
@@ -17,16 +8,43 @@ HOST_JENKINS_HOME=${cwd}
 fi
 
 MVN_COMMAND=$1
-if [ -z "${profile+xxx}" ];
+if [ -z "${MVN_COMMAND+xxx}" ];
 then
-#MVN_COMMAND=clean verify -P all-tests
-MVN_COMMAND=clean install
+MVN_COMMAND=clean install -Dmaven.test.skip=true 
 fi
 
-echo docker run --name=AristotleBuild -e MVN_COMMAND="$MVN_COMMAND" -e BUILD_OUTPUT=$BUILD_OUTPUT_DIRECTORY -v $HOST_JENKINS_HOME/workspace/${JOB_NAME}/build-output:$BUILD_OUTPUT_DIRECTORY -v ~/.m2:/root/.m2/ -e GIT_REPO=https://github.com/ModernAristotle/aristotle.git --net=host ping2ravi/maven-build-docker:latest
+echo "Build Output Directory : " $HOST_JENKINS_HOME/workspace/${JOB_NAME}/build-output
+docker run -e GIT_REPO=https://github.com/ModernAristotle/aristotle.git -e MVN_COMMAND='clean install -Dmaven.test.skip=true' -e BUILD_OUTPUT=$BUILD_OUTPUT_DIRECTORY -v $HOST_JENKINS_HOME/workspace/${JOB_NAME}/build-output:$BUILD_OUTPUT_DIRECTORY -v ~/.m2:/root/.m2/ -v /var/run/docker.sock:/var/run/docker.sock -it ping2ravi/delete-build
 
-docker run --name=AristotleBuild -e MVN_COMMAND="$MVN_COMMAND" -e BUILD_OUTPUT=$BUILD_OUTPUT_DIRECTORY -v $HOST_JENKINS_HOME/workspace/${JOB_NAME}/build-output:$BUILD_OUTPUT_DIRECTORY -v ~/.m2:/root/.m2/ -e GIT_REPO=https://github.com/ModernAristotle/aristotle.git --net=host ping2ravi/maven-build-docker:latest
- 
-docker stop mysql-for-build-test
-docker rm mysql-for-build-test
-docker rm AristotleBuild
+cd $HOST_JENKINS_HOME/workspace/${JOB_NAME}/build-output/member/member/
+
+mvn docker:build
+
+export MYSQL_ROOT_PASSWORD=password
+export MYSQL_DATABASE=ss_db_central
+export MYSQL_PORT=3305
+export MYSQL_DBSERVER=jdbc:mysql://localhost:${MYSQL_PORT}/${MYSQL_DATABASE}?characterEncoding=UTF-8
+
+#start MySql
+docker stop AristotleMysql
+docker rm AristotleMysql
+docker run --name=AristotleMysql -v ~/work/data/mysql/ss_db_central_temp:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} -e MYSQL_DATABASE=${MYSQL_DATABASE} -p ${MYSQL_PORT}:3306  -d mysql:5.5
+
+
+#start Member Docker image
+docker stop AristotleMemberBuild
+docker rm AristotleMemberBuild
+
+docker run --name=AristotleMemberBuild --net="host" -e user_search_end_point=${user_search_end_point} -e user_document_end_point=${user_document_end_point} -e dbuser=root -e dbpassword=${MYSQL_ROOT_PASSWORD} -e dbserver=${MYSQL_DBSERVER} -e user_cloud_search_domain_name=${user_cloud_search_domain_name} -e aws_region=${aws_region} -e aws_access_key=${aws_access_key} -e aws_access_secret=${aws_access_secret} -e planned_tweet_queue=${planned_tweet_queue} -e donation_queue_name=${donation_queue_name} -e poll_vote_queue_name=${poll_vote_queue_name} -e donation_queue_region=${donation_queue_region} -e instamojoApiKey=${instamojoApiKey} -e instamojoApiAuthToken=${instamojoApiAuthToken} -e donation_cloud_search_domain_name=${donation_cloud_search_domain_name} -e refresh_donation_queue=${refresh_donation_queue} -e refresh_user_queue=${refresh_user_queue} -e registration_mail_id=${registration_mail_id} -e google_api_key=${google_api_key} -e smsTransactionalUrlTemplate=${smsTransactionalUrlTemplate} -e smsPromotionalUrlTemplate=${smsPromotionalUrlTemplate} -e captcha_site_key=${captcha_site_key} -e captcha_site_secret=${captcha_site_secret} -d ping2ravi/member
+
+
+#Run tests in Docker Image
+docker stop AristotleMemberTestBuild
+docker rm AristotleMemberTestBuild
+docker run --name=AristotleMemberTestBuild --net="host" -e GIT_REPO=https://github.com/ModernAristotle/aristotle.git -e MVN_COMMAND='clean install' -e BUILD_OUTPUT=$BUILD_OUTPUT_DIRECTORY -v $HOST_JENKINS_HOME/workspace/${JOB_NAME}/build-output:$BUILD_OUTPUT_DIRECTORY -v ~/.m2:/root/.m2/ -v /var/run/docker.sock:/var/run/docker.sock -it ping2ravi/delete-build
+docker stop AristotleMemberTestBuild
+docker rm AristotleMemberTestBuild
+
+
+docker stop AristotleMemberBuild
+docker rm AristotleMemberBuild
