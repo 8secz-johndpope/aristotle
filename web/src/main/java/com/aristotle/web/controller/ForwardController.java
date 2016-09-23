c510a9390fee42d5e44d00a07201ba3731956327
Ravi Sharma <ping2ravi@gmail.com>
2016-09-23 09:23:17 +0530

    Fixed forward

--- web/src/main/java/com/aristotle/web/controller/ForwardController.java
+++ web/src/main/java/com/aristotle/web/controller/ForwardController.java
@@ -1,23 +1,13 @@
 package com.aristotle.web.controller;
 
-import com.aristotle.core.exception.AppException;
-import com.aristotle.core.service.UserService;
-import com.aristotle.core.service.VideoDownloader;
-import com.aristotle.core.service.temp.LocationUpgradeService;
-import com.aristotle.web.plugin.PluginManager;
-import com.aristotle.web.ui.template.UiTemplateManager;
-import com.github.jknack.handlebars.Handlebars;
-import com.github.jknack.handlebars.Template;
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class ForwardController {
@@ -30,7 +20,7 @@ public class ForwardController {
 
     @RequestMapping(value = {"/register"})
     public ModelAndView registerRedirect(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, ModelAndView modelAndView) {
-        RedirectView rv = new RedirectView("/index.html?register=1");
+        RedirectView rv = new RedirectView("https://member.swarajabhiyan.org/#!register");
         rv.setExposeModelAttributes(false);
         modelAndView.setView(rv);
         return modelAndView;
